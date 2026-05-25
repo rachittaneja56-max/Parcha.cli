c226c38647d57586757aafb82c257e009cbad5e2
rachittaneja56-max <rachittaneja56@gmail.com>
2026-05-26 01:56:30 +0530

    fixed issue in module import

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["./src/index.ts"],
+  format: ["esm"],
+  platform: "node",
+  target: "node18",
+  outExtension: () => ({ js: ".mjs" }),
   noExternal: ["@repo"], // transpile packages starting with `@repo` and their dependencies
   splitting: false,
   bundle: true,
